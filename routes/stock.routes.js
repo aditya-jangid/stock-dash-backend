f3fc01c9b1222ae19a34ea4f6d96ede73cd57816
aditya-jangid <jangid.aditya25@gmail.com>
2020-09-27 05:10:53 +0530

    added chart API

--- routes/stock.routes.js
+++ routes/stock.routes.js
@@ -5,7 +5,7 @@ const stockRoute = express();
 let myStockData = require('../model/stockschema');
 
 // Get all Stock
-stockRoute.route('/').get((req, res) => {
+stockRoute.route('/').get(async (req, res) => {
     myStockData.find((error, data) => {
         if (error) {
             return next(error)
@@ -16,9 +16,43 @@ stockRoute.route('/').get((req, res) => {
 })
 
 stockRoute.get('/by/:symbol', async (req, res) => {
-    const allData = await myStockData.find({"symbol":req.params.symbol});
+    const allData = await myStockData.find({ "symbol": req.params.symbol });
     try {
         res.send(allData);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
+
+stockRoute.get('/chartDataby/:symbol', async (req, res) => {
+    const allData = await myStockData.find({ "symbol": req.params.symbol });
+    try {
+        newData = JSON.stringify(allData, ['timeseries', 'date', 'predicted', 'close']);
+        newData = JSON.parse(newData);
+        newData = newData[0].timeseries;
+
+        // let dates = [];
+        // let closeValues = [];
+
+        // newData.map((item) => {
+        //     dates.push(item.date);
+        //     closeValues.push(item.close);
+        //   });
+        
+        var predictedObj = {
+            name : "predicted",
+            series: []
+        };
+
+        newData.map(function(item) {        
+            predictedObj.series.push({ 
+                 "name" : item.date.slice(0,10),
+                 "value"  : item.close
+             });
+         });
+
+
+        res.send(predictedObj);
     } catch (err) {
         res.status(500).send(err);
     }
